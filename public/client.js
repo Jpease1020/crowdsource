5412c6535c3e23f6aa8932e3d1953e531fde7acf
Justin Pease <justinpease2@gmail.com>
2016-04-21 15:38:47 -0600

    user can submit a new poll and info is sent to the server

--- public/client.js
+++ public/client.js
@@ -1,44 +1,36 @@
 var socket = io();
-
 var connectionCount = document.getElementById('connection-count');
 
 socket.on('usersConnected', function (count) {
   console.log(count)
 });
 
-var buttons = document.querySelectorAll('#choices button');
+// var buttons = document.querySelectorAll('#choices button');
 var $optionsContainer = $('.poll-options');
+var $addOption = $('.add-option');
+var $removeOption = $('.remove-option');
 
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-    console.log(this.innerText)
-    socket.send('voteCast', this.innerText);
-    var yourVote = document.getElementById('your-vote')
-    yourVote.innerText = this.innerText
-  });
-};
-
-$('.add-option').on('click', function(event){
+$addOption.on('click', function(event){
   event.preventDefault();
   $optionsContainer.append('<input class="option" type="text" placeholder="poll option"></input>')
 });
 
-$('.remove-option').on('click', function(event){
+$removeOption.on('click', function(event){
   event.preventDefault();
   $('.option').last().remove()
 });
 
-$('.create-new-poll').on('click', function(){
-  var $pollName = $('#poll-name').val()
-  var $option1 = $('#option-1').val()
-  var $option2 = $('#option-2').val()
-  var $option3 = $('#option-3').val()
-  var newPollInfo = {pollName: $pollName,
-                     option1: $option1,
-                     option2: $option2,
-                     option3: $option3,
-                   };
+$('.submit-poll').on('click', function(){
+  event.preventDefault();
+  var $pollName = $('#poll-name').val();
+  var newPollInfo = { pollName: $pollName };
+
+  var options = $('.option').map(function(index, element) {
+    newPollInfo['option' + (index + 1)] = $(element).val()
+  });
   socket.send('newPoll', newPollInfo);
+});
+  //
   // collect the input info
   // send the info to server
   // server side make new urls
@@ -46,4 +38,3 @@ $('.create-new-poll').on('click', function(){
   // new poll object should have the urls and the poll
   // make a new poll page
   // send the new urls back to the client
-});
