02b91dea8fa796e10a9865bc2b1a6f341ac2b06e
Justin Pease <justinpease2@gmail.com>
2016-04-24 15:00:46 -0600

    changes var polls to app locals polls

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/vote/:id", function(req, res){
 });
 
 function getPollByParamsId(req){
-  var poll = allPolls[req.params.id]
+  var poll = app.locals.allPolls[req.params.id]
   return poll
 };
 
@@ -64,7 +64,7 @@ io.on('connection', function (socket) {
   });
 });
 
-var allPolls = {}
+app.locals.allPolls = {}
 var activePolls = []
 
 function createNewPoll(channel, message, socket){
@@ -78,7 +78,7 @@ function createNewPoll(channel, message, socket){
   newPoll.pollInfo = message
   newPoll.votes = {}
 
-  allPolls[randomId] = newPoll
+  app.locals.allPolls[randomId] = newPoll
   activePolls.push(newPoll)
   socket.emit('newPoll', newPoll)
   };
@@ -93,11 +93,11 @@ function createUrls(randomId){
 function pollVotes(channel, incommingPoll, socket){
   if(channel === "voteCast"){
     var pollId = incommingPoll.pollId
-    if(allPolls[pollId]['pollInfo']['active']){
-       allPolls[pollId]['votes'][socket.id] = incommingPoll.vote
+    if(app.locals.allPolls[pollId]['pollInfo']['active']){
+       app.locals.allPolls[pollId]['votes'][socket.id] = incommingPoll.vote
      };
-    var individualPollChannel = allPolls[pollId]['pollId']
-    var organizedPoll = organizePoll(allPolls[pollId]['votes'])
+    var individualPollChannel = app.locals.allPolls[pollId]['pollId']
+    var organizedPoll = organizePoll(app.locals.allPolls[pollId]['votes'])
     io.sockets.emit(individualPollChannel, organizedPoll)
   };
 };
@@ -109,7 +109,7 @@ function organizePoll(uglyPoll){
 
 function closePoll(channel, pollId){
   if(channel === "close-poll"){
-    allPolls[pollId]['pollInfo']['active'] = false
+    app.locals.allPolls[pollId]['pollInfo']['active'] = false
     io.sockets.emit("pollEnded", {})
   }
 };
