58038b311950f561ab74a86afc51f70fa398ba5c
Justin Pease <justinpease2@gmail.com>
2016-04-25 11:49:23 -0600

    pulls setInterval into moduel

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const ejs = require('ejs')
+
 const createNewPoll = require('./lib/poll-creator')
 const getPollByParamsId = require('./lib/pole-finder')
 const pollVotes = require('./lib/poll-votes')
 const closePoll = require('./lib/close-poll')
+const checkForClosedPolls = require('./lib/check-for-closed-polls')
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app)
@@ -57,17 +59,6 @@ io.on('connection', function (socket) {
   });
 });
 
-setInterval(function() {
-    for(var i = 0; i <= app.locals.activePolls.length -1; i++){
-      var pollInfo = app.locals.activePolls[i]['pollInfo']
-      var pollDuration = pollInfo['pollDuration'] || 1440
-      var pollEndTime = pollInfo['startTime'] + (pollDuration * 60000)
-      if(pollEndTime < Date.now()){
-        pollInfo['active'] = false
-        app.locals.activePolls.splice(app.locals.activePolls.indexOf(app.locals.activePolls[i]), 1)
-        io.sockets.emit("pollEnded", {})
-      };
-    };
-}, 1000);
+checkForClosedPolls(app, io)
 
 module.exports = app;
